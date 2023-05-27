9995b9c7befcd3e3c6b66b55422c90619aa3f146
statkwon <statkwon96@gmail.com>
2023-05-27 21:01:20 +0900

    [ADD] add MemberController

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,18 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller // 스프링 컨테이너에 MemberController 객체를 생성하여 넣어둠
+// @Component로도 컴포넌트 스캔이 가능 -> @Controller, @Service, @Repository는 @Component를 포함
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 여러 컨트롤러에서 MemberService를 공유할 수 있으므로 매번 새로운 인스턴스를 생성할 필요가 없음 -> 스프링 컨테이너에 MemberService를 등록하여 사용
+    @Autowired // MemberController가 생성될 때 생성자에 @Autowired가 붙어 있으면 스프링 컨테이너 안에 있는 MemberService를 연결해 줌
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
