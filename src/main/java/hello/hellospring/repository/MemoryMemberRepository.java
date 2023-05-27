2bd384177d5f17e4c7bc0c0b47a5bbcfb7038cb6
statkwon <statkwon96@gmail.com>
2023-05-27 21:00:57 +0900

    [CHANGE] add @Repository annotation to MemoryMemberRepository

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 // 메모리 기반 저장소이기 때문에 서버 연결이 중단되면 데이터가 전부 소멸됨
+@Repository // 스프링 컨테이너에 MemberRepository 객체를 생성하여 넣어둠
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 동시성 문제로 인해 ConcurrentHashMap 사용
