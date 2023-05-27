218715ebe7c2e049781de07f7ce89b4c52f599dd
statkwon <statkwon96@gmail.com>
2023-05-27 20:59:49 +0900

    [CHANGE ] add @Service annotation to MemberService

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,16 +3,20 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 // Service 클래스에서는 일반적으로 비즈니스 로직에 어울리는 메서드명을 사용
+@Service // 스프링 컨테이너에 MemberService 객체를 생성하여 넣어둠
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // MemberService의 constructor를 사용하여 외부에서 MemberRepository를 넣어주는 것을 DI(Dependency Injection)이라 한다.
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
